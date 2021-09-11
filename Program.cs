7f7e6095d57a9ba475503078fb7f40c328f2412d
Kye <36981621+kyeondiscord@users.noreply.github.com>
2021-09-11 10:09:00 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+namespace FortniteExternalConsole
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Title = "Fortnite";
+            string logpath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\FortniteGame\Saved\Logs\FortniteGame.log";
+            using (StreamReader streamReader = new StreamReader(File.Open(logpath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)))
+            {
+                while (true)
+                {
+                    if (!streamReader.EndOfStream)
+                    {
+                        string readtoend = streamReader.ReadToEnd();
+                        if (readtoend.Length > 1)
+                            Console.WriteLine(readtoend);
+                        System.Threading.Thread.Sleep(100);
+                    }
+                }
+            }
+        }
+    }
+}
