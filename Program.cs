b7db6587bb763db2cb4c3bec79eb95f6f1fce835
Kye <36981621+KyeOnDiscord@users.noreply.github.com>
2022-09-29 14:09:45 +1000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 namespace FortniteExternalConsole
 {
     class Program
@@ -8,19 +9,25 @@ namespace FortniteExternalConsole
         {
             Console.Title = "Fortnite";
             string logpath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\FortniteGame\Saved\Logs\FortniteGame.log";
-            using (StreamReader streamReader = new StreamReader(File.Open(logpath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)))
+            if (File.Exists(logpath))
             {
-                while (true)
+                using (StreamReader streamReader = new StreamReader(File.Open(logpath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)))
                 {
-                    if (!streamReader.EndOfStream)
+                    while (true)
                     {
                         string readtoend = streamReader.ReadToEnd();
                         if (readtoend.Length > 1)
                             Console.WriteLine(readtoend);
-                        System.Threading.Thread.Sleep(100);
+                        Thread.Sleep(500);
                     }
                 }
             }
+            else
+            {
+                Console.WriteLine($"Could not find Fortnite log files at {logpath}");
+                Console.ReadKey(true);
+            }
+            
         }
     }
 }
